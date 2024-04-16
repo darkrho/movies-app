a336bcc35a56d28026a2d74f378f094e440c1f0b
darkrho <darkrho@outlook.es>
2024-04-16 10:08:54 +0200

    input part

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
+import { StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
+
+function App() {
+  const [value, onChangeText] = useState('')
+  const [modalVisible, setModeViseble] = useState(false)
+  const [searchResult, setResearchResult] = useState({})
+  const [isLoading, setLoadingVisble] = useState(false)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>IMDb API</Text>
+        <Text>The Open Movie Database</Text>
+        <TextInput 
+          style={styles.input}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+      </View>
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 48,
+    fontWeight: "bold"
+  }
 });
+
+export default App;
